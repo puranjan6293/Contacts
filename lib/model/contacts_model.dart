4b7707a14422b99cefecb2f14df311581b68c0a8
puranjan <mallik.puranjan@gmail.com>
2023-05-03 21:39:21 +0530

    comments removed

--- lib/model/contacts_model.dart
+++ lib/model/contacts_model.dart
@@ -1,27 +1,3 @@
-// class Contact {
-//   final String id;
-//   final String name;
-//   final String phone;
-
-//   Contact({required this.id, required this.name, required this.phone});
-
-//   factory Contact.fromJson(Map<String, dynamic> json) {
-//     return Contact(
-//       id: json['id'],
-//       name: json['name'],
-//       phone: json['phone'],
-//     );
-//   }
-
-//   Map<String, dynamic> toJson() {
-//     return {
-//       'id': id,
-//       'name': name,
-//       'phone': phone,
-//     };
-//   }
-// }
-
 class Contact {
   final String id;
   final String name;
